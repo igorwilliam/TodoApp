303612ad3a549f9979b0fd03683c8a705d3c843e
Igor William <igorwm9@gmail.com>
2019-03-19 08:26:40 -0300

    first commit

---
+++ backend/src/config/server.js
@@ -0,0 +1,16 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const AllowCors = require('./cors')
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(AllowCors)
+
+server.listen(port, function(){
+    console.log(`Backend rodando na porta ${port}`)
+})
+
+module.exports = server
---
+++ frontendRedux/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'/>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor incidunt tempora repellat repudiandae voluptates soluta, quae harum iure ullam laborum. Ipsam, a et nihil quasi consectetur libero tenetur aliquam eligendi.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores et ut voluptates deleniti quidem consectetur inventore unde saepe sit aut voluptatibus fugiat ipsam cumque numquam quo quia reprehenderit, aliquam sint?</p>
+        <h2>Impresa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ipsa non, exercitationem dolor culpa similique doloremque distinctio ab eaque veritatis eos aspernatur porro nostrum cum incidunt corporis sit illum tempore?</p>    
+    </div>
+)
